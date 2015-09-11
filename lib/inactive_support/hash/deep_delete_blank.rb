1e1f9980a3231d9a1d720848bead6477c7d4179e
gmitrev <gvmitrev@gmail.com>
2015-09-11 20:33:21 +0300

    Separate Hash#delete_blank and Hash#deep_delete_blank

---
+++ lib/inactive_support/hash/deep_delete_blank.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+class Hash
+  # Recursively deletes all key/value pairs where the value is empty string/array/hash or nil.
+  def deep_delete_blank
+    delete_if do |_, v|
+      (v.blank? && v != false) || v.instance_of?(Hash) && v.deep_delete_blank.empty?
+    end
+  end
+end
